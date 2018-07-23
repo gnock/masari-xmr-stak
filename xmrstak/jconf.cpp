1dfeefae189cba59e13071c75d54f3ecd1e29826
Interchained <president@worldvaporexpo.com>
2018-07-23 19:37:17 -0400

    Update jconf.cpp

--- xmrstak/jconf.cpp
+++ xmrstak/jconf.cpp
@@ -100,6 +100,7 @@ xmrstak::coin_selection coins[] = {
 	{ "cryptonight_v7",      {cryptonight_monero, cryptonight_monero, 0u}, {cryptonight_monero, cryptonight_monero, 0u}, nullptr },
 	{ "cryptonight_v7_stellite", {cryptonight_monero, cryptonight_stellite, 255u}, {cryptonight_monero, cryptonight_monero, 255u}, nullptr },
 	{ "edollar",             {cryptonight_monero, cryptonight, 255u},      {cryptonight_monero, cryptonight_monero, 0u}, nullptr },
+	{ "electronero",    {cryptonight_fast, cryptonight_fast, 7u},     {cryptonight_fast, cryptonight_fast, 0u},     "donate.electronero.org:1122" },
 	{ "electroneum",         {cryptonight_monero, cryptonight, 255u},      {cryptonight_monero, cryptonight_monero, 0u}, nullptr },
 	{ "graft",               {cryptonight_monero, cryptonight, 8u},        {cryptonight_monero, cryptonight_monero, 0u}, nullptr },
 	{ "haven",               {cryptonight_heavy, cryptonight, 2u},         {cryptonight_heavy, cryptonight_heavy, 0u},   nullptr },
